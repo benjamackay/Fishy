121932d3261a6c1bb0d533a754c0e7021ceb4002
benjamackay <bmackay@alumnos.inf.utfsm.cl>
2026-08-09 17:58:53 -0400

    Creación de manager de inventario

---
+++ Fishy!/Assets/Scripts/Inventario/InventoryManagerUI.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryManagerUI : MonoBehaviour
+{
+    public GameObject itemSlotPrefab; //gameobject instanciar
+    public Transform inventoryContainer; //sólo generar un hijo suyo
+}
